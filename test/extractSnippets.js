9e1856e0f095e09c6268879848f18b8347a90faa
Andreas Lind <andreaslindpetersen@gmail.com>
2019-01-10 21:17:18 +0100

    Allow code block modifiers/flags in HTML comments
    Fixes #12
    
    Also reduce duplication by using extractSnippets in convertMarkdownToMocha

--- test/extractSnippets.js
+++ test/extractSnippets.js
@@ -28,6 +28,30 @@ describe('extractSnippets', function() {
     ]);
   });
 
+  it('should default to evaluate:true', function() {
+    expect(extractSnippets('```js\nalert("Hello!");\n```\n'), 'to satisfy', [
+      { flags: { evaluate: true } }
+    ]);
+  });
+
+  it('should allow changing evaluate to false via js#evaluate:false', function() {
+    expect(
+      extractSnippets('```js#evaluate:false\nalert("Hello!");\n```\n'),
+      'to satisfy',
+      [{ flags: { evaluate: false } }]
+    );
+  });
+
+  it('should allow changing evaluate to false via a preceding HTML comment', function() {
+    expect(
+      extractSnippets(
+        '<!-- evaluate:false -->\n```js\nalert("Hello!");\n```\n'
+      ),
+      'to satisfy',
+      [{ flags: { evaluate: false } }]
+    );
+  });
+
   it('should extract a flag after the language specifier and #', function() {
     expect(
       extractSnippets('```js#foo:true\nalert("Hello!");\n```\n'),
@@ -41,6 +65,72 @@ describe('extractSnippets', function() {
       extractSnippets('```js#foo:true,bar:false\nalert("Hello!");\n```\n'),
       'to satisfy',
       [{ flags: { foo: true, bar: false } }]
+    );
+  });
+
+  it('should extract a flag from a preceding HTML comment', function() {
+    expect(
+      extractSnippets('<!-- foo:true -->\n```js\nalert("Hello!");\n```\n'),
+      'to satisfy',
+      [{ flags: { foo: true } }]
+    );
+  });
+
+  it('should extract multiple comma-separated flags from a preceding HTML comment', function() {
+    expect(
+      extractSnippets(
+        '<!-- foo:true,bar:false -->\n```js\nalert("Hello!");\n```\n'
+      ),
+      'to satisfy',
+      [{ flags: { foo: true, bar: false } }]
+    );
+  });
+
+  it('should tolerate whitespace between the flags and commans in the preceding HTML comment', function() {
+    expect(
+      extractSnippets(
+        '<!-- foo : true , bar : false -->\n```js\nalert("Hello!");\n```\n'
+      ),
+      'to satisfy',
+      [{ flags: { foo: true, bar: false } }]
+    );
+  });
+
+  it('should extract flags from both a preceding HTML command and after the language specifier and #', function() {
+    expect(
+      extractSnippets(
+        '<!-- foo:true -->\n```js#bar:false\nalert("Hello!");\n```\n'
+      ),
+      'to satisfy',
+      [{ flags: { foo: true, bar: false } }]
+    );
+  });
+
+  it('should let the flags after the language specifier win when the same flag is provided in both places', function() {
+    expect(
+      extractSnippets(
+        '<!-- foo:true -->\n```js#foo:false\nalert("Hello!");\n```\n'
+      ),
+      'to satisfy',
+      [{ flags: { foo: false } }]
+    );
+  });
+
+  it('provides the index of the block', function() {
+    expect(
+      extractSnippets('foobar\n```js#foo:false\nalert("Hello!");\n```\n'),
+      'to satisfy',
+      [{ index: 7 }]
+    );
+  });
+
+  it('skips past a preceding HTML comment when providing the index', function() {
+    expect(
+      extractSnippets(
+        'foobar\n<!-- foo: bar -->\n```js#foo:false\nalert("Hello!");\n```\n'
+      ),
+      'to satisfy',
+      [{ index: 25 }]
     );
   });
 });
