e57d12ffc7ba7bf8cd89c7b08fdfd151d9bd4316
Alex J Burke <alex@alexjeffburke.com>
2020-01-02 14:21:56 +0100

    Cover Snippets#getTests() method.

---
+++ test/Snippets.spec.js
@@ -0,0 +1,36 @@
+const expect = require('unexpected');
+
+const Snippets = require('../lib/Snippets');
+
+describe('Snippets', () => {
+  describe('#getTests()', () => {
+    it('should test block for each javascript/output pair', async () => {
+      const snippets = Snippets.fromMarkdown(
+        [
+          'Asserts deep equality.',
+          '',
+          '```javascript',
+          'throw new Error("foo\\n  at bar (/somewhere.js:1:2)\\n  at quux (/blah.js:3:4)\\n  at baz (/yadda.js:5:6)")',
+          '```',
+          '',
+          '<!-- unexpected-markdown cleanStackTrace:true -->',
+          '```output',
+          'foo',
+          '  at bar (/path/to/file.js:x:y)',
+          '  at quux (/path/to/file.js:x:y)',
+          '```'
+        ].join('\n')
+      );
+
+      expect(await snippets.getTests(), 'to equal', [
+        {
+          code:
+            'throw new Error("foo\\n  at bar (/somewhere.js:1:2)\\n  at quux (/blah.js:3:4)\\n  at baz (/yadda.js:5:6)")',
+          flags: { evaluate: true },
+          output:
+            'foo\n  at bar (/path/to/file.js:x:y)\n  at quux (/path/to/file.js:x:y)'
+        }
+      ]);
+    });
+  });
+});
