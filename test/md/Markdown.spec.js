2487e5f0ea1823a5820eec38eafdb594863360a0
Alex J Burke <alex@alexjeffburke.com>
2020-03-25 23:52:10 +0100

    Rework Markdown spec file.

--- test/md/Markdown.spec.js
+++ test/md/Markdown.spec.js
@@ -1,74 +1,125 @@
-var expect = require("unexpected");
+var expect = require("unexpected")
+  .clone()
+  .use(require("unexpected-snapshot"));
 var Markdown = require("../../lib/md/Markdown");
 
-describe("Markdown", function() {
-  var markdown;
-  beforeEach(function() {
-    markdown = new Markdown(
-      [
-        "Asserts deep equality.",
-        "",
-        "```javascript",
-        "expect({ a: 'b' }, 'to equal', { a: 1234 });",
-        "var now = new Date();",
-        "expect(now, 'to equal', now);",
-        "expect(now, 'to equal', new Date(now.getTime()));",
-        "expect({ now: now }, 'to equal', { now: now });",
-        "```",
-        "",
-        "For a lot of types a failing equality test results in a nice",
-        "diff. Below you can see an object diff.",
-        "",
-        "```javascript",
-        "expect({ text: 'foo!' }, 'to equal', { text: 'f00!' });",
-        "```",
-        "",
-        "```output",
-        "Missing output",
-        "```"
-      ].join("\n")
-    );
-  });
+const codeBlockWithSkipped = [
+  "Asserts deep equality.",
+  "",
+  "```javascript",
+  "expect({ a: 'b' }, 'to equal', { a: 1234 });",
+  "var now = new Date();",
+  "expect(now, 'to equal', now);",
+  "expect(now, 'to equal', new Date(now.getTime()));",
+  "expect({ now: now }, 'to equal', { now: now });",
+  "```",
+  "",
+  "For a lot of types a failing equality test results in a nice",
+  "diff. Below you can see an object diff.",
+  "",
+  "```javascript",
+  "expect({ text: 'foo!' }, 'to equal', { text: 'f00!' });",
+  "```",
+  "",
+  "```output",
+  "Missing output",
+  "```"
+].join("\n");
 
+function locateAndReturnOutputBlock(output) {
+  const blockIndex = output.indexOf("```output");
+  if (blockIndex === -1) {
+    throw new Error("unable to locate output block");
+  }
+  return output.slice(blockIndex);
+}
+
+describe("Markdown", function() {
   describe("toHtml", function() {
-    it("syntax highlight examples", function() {
+    it("should render a syntax highlighted code block", function() {
+      const markdown = new Markdown(
+        [
+          "```javascript",
+          "expect({ a: 'b' }, 'to equal', { a: 1234 });",
+          "```"
+        ].join("\n")
+      );
+
       return expect(
         markdown.toHtml(),
         "when fulfilled",
-        "to contain",
-        '<span style="color: #905">1234</span>'
+        "to equal snapshot",
+        '<div class="code lang-javascript"><div><span style="color: #DD4A68">expect</span><span style="color: #999">({</span>&nbsp;a<span style="color: #a67f59">:</span>&nbsp;<span style="color: #690">\'b\'</span>&nbsp;<span style="color: #999">},</span>&nbsp;<span style="color: #690">\'to&nbsp;equal\'</span><span style="color: #999">,</span>&nbsp;<span style="color: #999">{</span>&nbsp;a<span style="color: #a67f59">:</span>&nbsp;<span style="color: #905">1234</span>&nbsp;<span style="color: #999">});</span></div></div>'
+      );
+    });
+
+    it("should evaluate and render a syntax highlighted output block", function() {
+      const markdown = new Markdown(
+        [
+          "```javascript",
+          "expect({ a: 'b' }, 'to equal', { a: 1234 });",
+          "```",
+          "",
+          "```output",
+          "Missing output",
+          "```"
+        ].join("\n")
+      );
+
+      return expect(
+        markdown.toHtml(),
+        "when fulfilled",
+        expect
+          .it("not to contain", "Missing output")
+          .and("to contain", '<div class="output">')
+      );
+    });
+
+    it("should throw if an output block occurrs with no code block", function() {
+      const markdown = new Markdown(
+        ["```output", "Missing output", "```"].join("\n")
+      );
+
+      return expect(
+        () => markdown.toHtml(),
+        "to be rejected with",
+        expect.it(error =>
+          expect(
+            error.message,
+            "to start with",
+            "No matching javascript block for output:\nMissing output"
+          )
+        )
       );
     });
   });
 
   describe("withUpdatedExamples", function() {
-    it("produces a markdown where the examples has been updated", function() {
-      return expect(
-        markdown.withUpdatedExamples({}),
-        "when fulfilled",
-        expect.it(markdown =>
-          expect(
-            markdown.toString(),
-            "to contain",
-            [
-              "```output",
-              "expected { text: 'foo!' } to equal { text: 'f00!' }",
-              "",
-              "{",
-              "  text: 'foo!' // should equal 'f00!'",
-              "               //",
-              "               // -foo!",
-              "               // +f00!",
-              "}",
-              "```"
-            ].join("\n")
-          )
-        )
+    it("should produce updated markdown for an unexpected diff", async function() {
+      const markdown = await new Markdown(
+        codeBlockWithSkipped
+      ).withUpdatedExamples({});
+
+      expect(
+        locateAndReturnOutputBlock(markdown.toString()),
+        "to equal snapshot",
+        expect.unindent`
+          \`\`\`output
+          expected { text: 'foo!' } to equal { text: 'f00!' }
+
+          {
+            text: 'foo!' // should equal 'f00!'
+                         //
+                         // -foo!
+                         // +f00!
+          }
+          \`\`\`
+        `
       );
     });
 
-    it("should work correctly with async snippets that reject", async () => {
-      const markdown = new Markdown(
+    it("should produces updated markdown for async rejection", async function() {
+      const markdown = await new Markdown(
         [
           "<!-- unexpected-markdown async:true -->",
           "```javascript",
@@ -79,51 +130,44 @@ describe("Markdown", function() {
           "Missing output",
           "```"
         ].join("\n")
-      );
-
-      const updatedMarkdown = await markdown.withUpdatedExamples({});
+      ).withUpdatedExamples({});
 
       expect(
-        updatedMarkdown.content,
-        "to contain",
-        ["```output", "boom", "```"].join("\n")
+        locateAndReturnOutputBlock(markdown.toString()),
+        "to equal snapshot",
+        expect.unindent`
+          \`\`\`output
+          boom
+          \`\`\`
+        `
       );
     });
-  });
 
-  it("produces a markdown where the examples has been updated", function() {
-    const markdown = new Markdown(
-      [
-        "Asserts deep equality.",
-        "",
-        "```javascript",
-        'throw new Error("foo\\n  at bar (/somewhere.js:1:2)\\n  at quux (/blah.js:3:4)\\n  at baz (/yadda.js:5:6)")',
-        "```",
-        "",
-        "<!-- unexpected-markdown cleanStackTrace:true -->",
-        "```output",
-        "Missing output",
-        "```"
-      ].join("\n")
-    );
+    it("should produces updated markdown for sync throw", async function() {
+      const markdown = await new Markdown(
+        [
+          "```javascript",
+          'throw new Error("foo\\n  at bar (/somewhere.js:1:2)\\n  at quux (/blah.js:3:4)\\n  at baz (/yadda.js:5:6)")',
+          "```",
+          "",
+          "<!-- unexpected-markdown cleanStackTrace:true -->",
+          "```output",
+          "Missing output",
+          "```"
+        ].join("\n")
+      ).withUpdatedExamples({});
 
-    return expect(
-      markdown.withUpdatedExamples({}),
-      "when fulfilled",
-      expect.it(markdown =>
-        expect(
-          markdown.toString(),
-          "to contain",
-          [
-            "<!-- unexpected-markdown cleanStackTrace:true -->",
-            "```output",
-            "foo",
-            "  at bar (/path/to/file.js:x:y)",
-            "  at quux (/path/to/file.js:x:y)",
-            "```"
-          ].join("\n")
-        )
-      )
-    );
+      expect(
+        locateAndReturnOutputBlock(markdown.toString()),
+        "to equal snapshot",
+        expect.unindent`
+          \`\`\`output
+          foo
+            at bar (/path/to/file.js:x:y)
+            at quux (/path/to/file.js:x:y)
+          \`\`\`
+        `
+      );
+    });
   });
 });
