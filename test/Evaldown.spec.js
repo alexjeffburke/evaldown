1b0acfd43277d75826a6480a67084f802e921c90
Alex J Burke <alex@alexjeffburke.com>
2020-03-26 20:06:49 +0100

    Add snapshot tests capturing functional output formats and update.

--- test/Evaldown.spec.js
+++ test/Evaldown.spec.js
@@ -1,6 +1,7 @@
 const expect = require("unexpected")
   .clone()
-  .use(require("unexpected-sinon"));
+  .use(require("unexpected-sinon"))
+  .use(require("unexpected-snapshot"));
 const fsExtra = require("fs-extra");
 const path = require("path");
 const sinon = require("sinon");
@@ -11,6 +12,30 @@ const TESTDATA_PATH = path.join(__dirname, "..", "testdata");
 const TESTDATA_OUTPUT_PATH = path.join(TESTDATA_PATH, "output");
 
 describe("Evaldown", () => {
+  expect.addAssertion(
+    "<string> to be present on disk",
+    async (expect, subject) => {
+      await expect(
+        () => fsExtra.pathExists(subject),
+        "to be fulfilled with",
+        true
+      );
+    }
+  );
+
+  expect.addAssertion(
+    "<string> to be present on disk with content satisfying <assertion>",
+    async (expect, subject, value) => {
+      expect.errorMode = "nested";
+      await expect(subject, "to be present on disk");
+      const content = await fsExtra.readFile(subject, "utf8");
+      expect.errorMode = "nested";
+      await expect.shift(content);
+
+      return content;
+    }
+  );
+
   before(async () => {
     await fsExtra.ensureDir(TESTDATA_OUTPUT_PATH);
   });
@@ -101,9 +126,13 @@ describe("Evaldown", () => {
         "captured.html"
       );
       await expect(
-        () => fsExtra.pathExists(expectedOutputFile),
-        "to be fulfilled with",
-        true
+        expectedOutputFile,
+        "to be present on disk with content satisfying",
+        "to equal snapshot",
+        expect.unindent`
+          <p>Testing output capturing.</p>
+          <div class="code lang-javascript"><div><span style="color: #07a">function</span>&nbsp;<span style="color: #DD4A68">doSomething</span><span style="color: #999">()</span>&nbsp;<span style="color: #999">{</span></div><div>&nbsp;&nbsp;<span style="color: #07a">return</span>&nbsp;<span style="color: #999">{</span>&nbsp;foo<span style="color: #a67f59">:</span>&nbsp;<span style="color: #690">&quot;bar&quot;</span>&nbsp;<span style="color: #999">};</span></div><div><span style="color: #999">}</span></div><div>&nbsp;</div><div><span style="color: #708090">//&nbsp;objects&nbsp;are&nbsp;inspected&nbsp;too</span></div><div><span style="color: #07a">return</span>&nbsp;<span style="color: #DD4A68">doSomething</span><span style="color: #999">();</span></div></div><div class="output"><div>{&nbsp;<span style="color: #555">foo</span>:&nbsp;<span style="color: #df5000">'bar'</span>&nbsp;}</div></div>
+        `
       );
     });
 
@@ -117,16 +146,10 @@ describe("Evaldown", () => {
       await evaldown.processFiles();
 
       await expect(
-        () =>
-          fsExtra.pathExists(path.join(TESTDATA_OUTPUT_PATH, "strings.html")),
-        "to be fulfilled with",
-        true
-      );
-      await expect(
-        () =>
-          fsExtra.pathExists(path.join(TESTDATA_OUTPUT_PATH, "objects.html")),
-        "to be fulfilled with",
-        true
+        path.join(TESTDATA_OUTPUT_PATH, "strings.html"),
+        "to be present on disk with content satisfying",
+        "to equal snapshot",
+        '<div style="font-family: monospace; white-space: nowrap"><div>hello,&nbsp;world!</div><div><span style="color: red; font-weight: bold">foobar</span></div></div>'
       );
     });
 
@@ -145,9 +168,13 @@ describe("Evaldown", () => {
         "nothing.html"
       );
       await expect(
-        () => fsExtra.pathExists(expectedOutputFile),
-        "to be fulfilled with",
-        true
+        expectedOutputFile,
+        "to be present on disk with content satisfying",
+        "to equal snapshot",
+        expect.unindent`
+          <p>Testing output capturing.</p>
+          <div class="code lang-javascript"><div><span style="color: #DD4A68">expect</span><span style="color: #999">(</span><span style="color: #690">&quot;f00&quot;</span><span style="color: #999">,</span>&nbsp;<span style="color: #690">&quot;to&nbsp;equal&quot;</span><span style="color: #999">,</span>&nbsp;<span style="color: #690">&quot;foo&quot;</span><span style="color: #999">);</span></div></div><div class="output"><div><span style="color: red; font-weight: bold">expected</span>&nbsp;<span style="color: #df5000">'f00'</span>&nbsp;<span style="color: red; font-weight: bold">to&nbsp;equal</span>&nbsp;<span style="color: #df5000">'foo'</span></div><div>&nbsp;</div><div><span style="background-color: red; color: white">f00</span></div><div><span style="background-color: green; color: white">foo</span></div></div>
+        `
       );
     });
   });
@@ -266,7 +293,7 @@ describe("Evaldown", () => {
   });
 
   describe("when operating in update mode", function() {
-    it("should glob for the supplied extension", async function() {
+    it("should make changes to the source markdown", async function() {
       const sourceFile = "expect.md";
       const sourceFilePath = path.join(TESTDATA_PATH, "example", sourceFile);
       const originalSource = await fsExtra.readFile(sourceFilePath, "utf8");
@@ -286,6 +313,58 @@ describe("Evaldown", () => {
           () => {
             expect.fail({ message: "The source file was not updated." });
           }
+        );
+      } finally {
+        await fsExtra.writeFile(sourceFilePath, originalSource, "utf8");
+      }
+    });
+
+    it("should output the correct updated markdown", async () => {
+      const sourceFile = "expect.md";
+      const sourceFilePath = path.join(TESTDATA_PATH, "example", sourceFile);
+      const originalSource = await fsExtra.readFile(sourceFilePath, "utf8");
+
+      await new Evaldown({
+        update: true,
+        sourcePath: path.dirname(sourceFilePath),
+        targetPath: TESTDATA_OUTPUT_PATH
+      }).processFile(sourceFile);
+
+      try {
+        await expect(
+          sourceFilePath,
+          "to be present on disk with content satisfying",
+          "to equal snapshot",
+          expect.unindent`
+            Asserts deep equality.
+
+            \`\`\`javascript
+            expect({ a: "b" }, "to equal", { a: 1234 });
+            var now = new Date();
+            expect(now, "to equal", now);
+            expect(now, "to equal", new Date(now.getTime()));
+            expect({ now: now }, "to equal", { now: now });
+            \`\`\`
+
+            For a lot of types a failing equality test results in a nice
+            diff. Below you can see an object diff.
+
+            \`\`\`javascript
+            expect({ text: "foo!" }, "to equal", { text: "f00!" });
+            \`\`\`
+
+            \`\`\`output
+            expected { text: 'foo!' } to equal { text: 'f00!' }
+
+            {
+              text: 'foo!' // should equal 'f00!'
+                           //
+                           // -foo!
+                           // +f00!
+            }
+            \`\`\`
+
+          `
         );
       } finally {
         await fsExtra.writeFile(sourceFilePath, originalSource, "utf8");
