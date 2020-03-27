f40f8ad6eb8c9f85d546dd27204afeda815e6342
Alex J Burke <alex@alexjeffburke.com>
2020-03-27 18:59:57 +0100

    Update Snippets tests.

--- test/md/Snippets.spec.js
+++ test/md/Snippets.spec.js
@@ -2,9 +2,50 @@ const expect = require("unexpected");
 
 const Snippets = require("../../lib/md/Snippets");
 
+const testSnippets = [
+  {
+    lang: "javascript",
+    flags: { evaluate: true },
+    index: 24,
+    code:
+      'throw new Error("foo\\n  at bar (/somewhere.js:1:2)\\n  at quux (/blah.js:3:4)\\n  at baz (/yadda.js:5:6)")'
+  },
+  {
+    lang: "output",
+    flags: { cleanStackTrace: true, evaluate: true },
+    index: 198,
+    code:
+      "foo\n  at bar (/path/to/file.js:x:y)\n  at quux (/path/to/file.js:x:y)"
+  }
+];
+
 describe("Snippets", () => {
+  it("should allow retrieving a snippet by index", () => {
+    const snippets = new Snippets(testSnippets);
+
+    expect(snippets.get(1), "to equal", testSnippets[1]);
+  });
+
   describe("#getTests()", () => {
-    it("should test block for each javascript/output pair", async () => {
+    it("should combine each code/output pair", () => {
+      const snippets = new Snippets(
+        testSnippets.map(snippet => ({ ...snippet, evaluate: false }))
+      );
+
+      expect(snippets.getTests(), "to satisfy", [
+        {
+          code:
+            'throw new Error("foo\\n  at bar (/somewhere.js:1:2)\\n  at quux (/blah.js:3:4)\\n  at baz (/yadda.js:5:6)")',
+          flags: { evaluate: true },
+          output:
+            "foo\n  at bar (/path/to/file.js:x:y)\n  at quux (/path/to/file.js:x:y)"
+        }
+      ]);
+    });
+  });
+
+  describe("Snippets.fromMarkdown()", () => {
+    it("should extract the snippets", () => {
       const snippets = Snippets.fromMarkdown(
         [
           "Asserts deep equality.",
@@ -22,15 +63,7 @@ describe("Snippets", () => {
         ].join("\n")
       );
 
-      expect(await snippets.getTests(), "to equal", [
-        {
-          code:
-            'throw new Error("foo\\n  at bar (/somewhere.js:1:2)\\n  at quux (/blah.js:3:4)\\n  at baz (/yadda.js:5:6)")',
-          flags: { evaluate: true },
-          output:
-            "foo\n  at bar (/path/to/file.js:x:y)\n  at quux (/path/to/file.js:x:y)"
-        }
-      ]);
+      expect(snippets.items, "to satisfy", testSnippets);
     });
   });
 });
