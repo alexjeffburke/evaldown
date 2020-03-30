b727755c31d1c59dcf3b6265d5db76be8364c6a9
Alex J Burke <alex@alexjeffburke.com>
2020-03-30 22:40:50 +0200

    Add processing of a single file in-place through into the CLI.
    Implement an updateFile() counterpart to processFile() which deals
    only with updating the source file and has no knowledge of a target
    file or directory.
    
    Use this within a new file command method in the CLI to support
    processing a single file - either generating the output or writing
    it in-place via updateFile().
    
    Modify the bin file to be aware of a positional file argument. If
    supplied it will invoke the single file CLI function and otherwise
    it will proceed with directory processing version as before.

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -1,6 +1,10 @@
-const expect = require("unexpected");
+const expect = require("unexpected")
+  .clone()
+  .use(require("unexpected-sinon"))
+  .use(require("unexpected-snapshot"));
 const fsExtra = require("fs-extra");
 const path = require("path");
+const sinon = require("sinon");
 
 const cli = require("../lib/cli");
 
@@ -69,6 +73,86 @@ describe("cli", () => {
         "to be rejected with",
         'Invalid "sourcePath"'
       );
+    });
+  });
+
+  describe("file()", () => {
+    it("should output markdown to stdout by default", async () => {
+      const pwd = path.join(TESTDATA_PATH, "extensions");
+      const cons = {
+        log: sinon.stub().named("log")
+      };
+
+      await cli.file(pwd, {
+        _cons: cons,
+        _: ["expect.markdown"]
+      });
+
+      await expect(cons.log, "to have a call satisfying", [
+        expect.it(
+          "to equal snapshot",
+          expect.unindent`
+            Some string manipulations.
+
+            \`\`\`javascript
+            return "foobar".slice(0, 3);
+            \`\`\`
+
+            \`\`\`output
+            foo
+            \`\`\`
+
+            \`\`\`javascript
+            return "foobar".slice(3, 6);
+            \`\`\`
+
+            \`\`\`output
+            bar
+            \`\`\`
+
+          `
+        )
+      ]);
+    });
+
+    it('should write an update the file when "inplace"', async () => {
+      const pwd = path.join(TESTDATA_PATH, "extensions");
+      const sourceFilePath = path.join(pwd, "expect.markdown");
+      const originalSource = await fsExtra.readFile(sourceFilePath, "utf8");
+
+      try {
+        await cli.file(pwd, {
+          update: true,
+          _: ["expect.markdown"]
+        });
+
+        await expect(
+          await fsExtra.readFile(sourceFilePath, "utf8"),
+          "to equal snapshot",
+          expect.unindent`
+            Some string manipulations.
+
+            \`\`\`javascript
+            return "foobar".slice(0, 3);
+            \`\`\`
+
+            \`\`\`output
+            foo
+            \`\`\`
+
+            \`\`\`javascript
+            return "foobar".slice(3, 6);
+            \`\`\`
+
+            \`\`\`output
+            bar
+            \`\`\`
+
+          `
+        );
+      } finally {
+        await fsExtra.writeFile(sourceFilePath, originalSource, "utf8");
+      }
     });
   });
 });
